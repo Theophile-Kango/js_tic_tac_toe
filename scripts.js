fac115d5e5530924b45cb8eda89ea99047070711
Theophile-Kango <fadhili.kango@gmail.com>
2020-06-12 11:47:24 +0200

    Added the switchPlayer function to switch role between Players

--- scripts.js
+++ scripts.js
@@ -15,15 +15,26 @@ const player2 = playerFactory(name2,'O');
 const gameButtons = document.querySelector('.gameboard');
 
 const game = (() => {
+  let current = true;
   const play = () => {
     const buttons = gameButtons.querySelectorAll('button');
     buttons.forEach((element, i) => {
       element.setAttribute('id',i);
-      element.addEventListener('click', ()=> {
-        element.textContent = player1.symbol;
+      element.addEventListener('click', () => {
+        element.textContent = switchPlayers(player1.symbol, player2.symbol);
       })
     })
   }
+  const switchPlayers = (player1, player2) => {
+    if (current == true){
+      current = false
+      return player1
+    }else{
+      current = true
+      return player2
+    }
+  }
+
   return {play}
 })();
 
