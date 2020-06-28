7aa0ebec80bae9cd6eb7a5ec12e6192a1106b226
Theophile-Kango <fadhili.kango@gmail.com>
2020-06-28 08:15:55 +0200

    Add test for gamePlayer module players function

--- index.test.js
+++ index.test.js
@@ -1,5 +1,15 @@
-import { game } from './src/index';
+import { gamePlayers } from './src/app/main';
 
-test('play', () => {
-  expect(game.play()).toBe(false);
+describe('gamePlayers', () => {
+  const players = gamePlayers.players('Theo','Murilo');
+  it("It takes the players's names, gives the symbols to each player and switch players", () => {
+    const {name, score, symbol} = players[0];
+    expect(typeof players).toEqual("object");
+    expect(name).toEqual("Theo");
+    expect(score).toEqual(0);
+    expect(players[1].symbol).toEqual("O");
+    expect(symbol).not.toEqual("O");
+    expect(players[1].name).not.toEqual("John");
+    expect(players[1].score).not.toEqual(1);
+  });
 });
